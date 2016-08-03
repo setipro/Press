2f8f457163ef2881f8e112a4586b1f77f9fc290c
bin <bin@bindeMacBook-Pro.local>
2016-08-03 16:26:51 +0800

    finish basic outlooking of MyViewController

--- Press/Class/AvatarTableViewCell.m
+++ Press/Class/AvatarTableViewCell.m
@@ -7,17 +7,40 @@
 //
 
 #import "AvatarTableViewCell.h"
+#import <Masonry/Masonry.h>
+
+@interface AvatarTableViewCell()
+@property (nonatomic, strong) UIImageView *avatarImageView;
+@property (nonatomic, strong) UILabel *nameLabel;
+@end
 
 @implementation AvatarTableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
+#pragma mark - life cycle
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.avatarImageView = [[UIImageView alloc] init];
+        self.avatarImageView.backgroundColor = [UIColor grayColor];
+        [self.contentView addSubview:self.avatarImageView];
+        [self.avatarImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.center.equalTo(self.contentView);
+            make.width.and.height.equalTo(@100);
+        }];
+        self.nameLabel = [[UILabel alloc] init];
+        self.nameLabel.text = @"用户名称";
+        self.nameLabel.tintColor = [UIColor blackColor];
+        self.nameLabel.textAlignment = NSTextAlignmentCenter;
+        [self.contentView addSubview:self.nameLabel];
+        [self.nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(self.avatarImageView);
+            make.top.equalTo(self.avatarImageView.mas_bottom).with.offset(20);
+            make.width.equalTo(self.avatarImageView);
+            make.height.equalTo(@44);
+        }];
+    }
+    return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 
 @end
--- Press/Class/MyViewController.m
+++ Press/Class/MyViewController.m
@@ -7,17 +7,67 @@
 //
 
 #import "MyViewController.h"
+#import "AvatarTableViewCell.h"
+#import "DiscoveryTableViewCell.h"
+#import <Masonry/Masonry.h>
 
-@interface MyViewController ()
-
+@interface MyViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation MyViewController
 
+#pragma mark - tableview delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        return 300;
+    }
+    return 100;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+#pragma mark - tableview datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 4;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        AvatarTableViewCell *cell = [[AvatarTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        return cell;
+    }
+    DiscoveryTableViewCell *cell = [[DiscoveryTableViewCell alloc] init];
+    cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    cell.tintColor = [UIColor blackColor];
+    if (indexPath.row == 1) {
+        cell.textLabel.text = @"我的收藏";
+    } else if (indexPath.row == 2) {
+        cell.textLabel.text = @"我的发布会";
+    } else {
+        cell.textLabel.text = @"我的密码";
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
+    self.tableView = [[UITableView alloc] init];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    self.tableView.backgroundView = nil;
+    self.tableView.backgroundColor = [UIColor clearColor];
+    self.tableView.separatorColor = [UIColor clearColor];
 }
 
 - (void)didReceiveMemoryWarning {

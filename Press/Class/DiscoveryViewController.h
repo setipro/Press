7b6d2907ca1e8f6205acd94f53dea9b595ceeb90
bin <bin@bindeMacBook-Pro.local>
2016-08-03 11:50:50 +0800

    add 3 main vc

---
+++ Press/Class/DiscoveryViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoveryViewController.h
+//  Press
+//
+//  Created by bin on 16/8/3.
+//  Copyright © 2016年 bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoveryViewController : UIViewController
+
+@end
---
+++ Press/Class/OpenViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpenViewController.h
+//  Press
+//
+//  Created by bin on 16/8/3.
+//  Copyright © 2016年 bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenViewController : UIViewController
+
+@end
